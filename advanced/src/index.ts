0d42be4900bacb799b2465584fedd0761d3e0011
Taras_Moskovych <ICTaras.Moskovych@amwell.com>
2021-11-04 13:23:51 +0200

    create custom observable

--- advanced/src/index.ts
+++ advanced/src/index.ts
@@ -1,4 +1,40 @@
 import './styles.css';
-import { add } from './helpers';
 
-add.li('RxJs');
+import { Observable } from 'rxjs';
+import { add } from './helpers';
+import { CustomObservable } from './observable';
+
+const promise = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve('Promise');
+  }, 2000);
+});
+
+const observable$ = new Observable((observer) => {
+  setTimeout(() => {
+    observer.next('Observable 1');
+    observer.complete();
+  }, 2000);
+});
+
+const customObservable$ = new CustomObservable((observer) => {
+  setTimeout(() => {
+    observer.next('CustomObservable');
+    observer.complete();
+    observer.next('CustomObservable');
+  }, 2000);
+});
+
+promise.then((message: string) => add.li(message));
+
+observable$.subscribe({
+  next: (message: string) => add.li(message),
+  error: (err: string) => console.error(err),
+  complete: () => add.li('Completed'),
+});
+
+customObservable$.subscribe(
+  (message: string) => add.li(message),
+  (err: string) => console.error(err),
+  () => add.li('Completed'),
+);
