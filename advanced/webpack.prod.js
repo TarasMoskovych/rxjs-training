e2b4178f2cd16234ea750c804d2f5f2ac8978334
Taras_Moskovych <ICTaras.Moskovych@amwell.com>
2021-11-18 16:50:51 +0200

    add css-minimizer plugin

--- advanced/webpack.prod.js
+++ advanced/webpack.prod.js
@@ -1,6 +1,12 @@
 const webpackCommon = require('./webpack.common');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
   ...webpackCommon,
   mode: 'production',
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin(),
+    ],
+  },
 };
