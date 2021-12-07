50f68101d919c355536c9e3569205b1147719c8d
Taras_Moskovych <ICTaras.Moskovych@amwell.com>
2021-12-07 16:41:06 +0200

    implement course detail

--- courses-app/src/app/core/services/courses.service.ts
+++ courses-app/src/app/core/services/courses.service.ts
@@ -20,16 +20,27 @@ export class CoursesService {
     );
   }
 
+  getById(id: string): Observable<Course> {
+    return this.http.get<Course>(`/api/courses/${id}`).pipe(
+      shareReplay(),
+    );
+  }
+
   update(id: string, changes: Partial<Course>): Observable<Course> {
     return this.http.put<Course>(`/api/courses/${id}`, changes);
   }
 
+  getLessonsByCourseId(courseId: string): Observable<Lesson[]> {
+    return this.getLessons({ pageSize: 200, courseId });
+  }
+
   searchLessons(search: string): Observable<Lesson[]> {
+    return this.getLessons({ pageSize: 100, filter: search });
+  }
+
+  private getLessons(params: any): Observable<Lesson[]> {
     return this.http.get<{ payload: Lesson[] }>(`/api/lessons`, {
-      params: {
-        filter: search,
-        pageSize: 100,
-      },
+      params,
     }).pipe(
       pluck('payload'),
       shareReplay(),
--- courses-app/src/app/course/course.component.ts
+++ courses-app/src/app/course/course.component.ts
@@ -1,18 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { combineLatest, map, Observable } from 'rxjs';
+import { CoursesService } from '../core';
 import { Course, Lesson } from '../shared/models';
+
+interface CourseData {
+  course: Course;
+  lessons: Lesson[];
+}
 
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
-  styleUrls: ['./course.component.scss']
+  styleUrls: ['./course.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CourseComponent implements OnInit {
-  course: Course;
-  lessons: Lesson[];
+  courseData$: Observable<CourseData>;
 
-  constructor() { }
+  constructor(
+    private coursesService: CoursesService,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
-  }
+    const { courseId } = this.route.snapshot.params;
 
+    this.courseData$ = combineLatest([
+      this.coursesService.getById(courseId),
+      this.coursesService.getLessonsByCourseId(courseId),
+    ]).pipe(
+      map(([course, lessons]) => ({ course, lessons })),
+    );
+  }
 }
